45153594cea3565824f59edb2e096e54e650bc13
Austin McCallister <austin.mccallister@protonmail.com>
2023-11-17 15:39:38 -0500

    Convert if statement to guard clause to reduce nesting

--- substrings.rb
+++ substrings.rb
@@ -2,9 +2,9 @@
 
 def substrings(string, dictionary)
   dictionary.each_with_object({}) do |substring, hash|
-    if string.downcase.include?(substring.downcase)
-      # Positive lookahead prevents consuming characters in another valid substring
-      hash[substring] = string.downcase.scan(/(?=#{substring.downcase})/).count
-    end
+    next unless string.downcase.include?(substring.downcase)
+
+    # Positive lookahead prevents consuming characters in another valid substring
+    hash[substring] = string.downcase.scan(/(?=#{substring.downcase})/).count
   end
 end
